4855ae1c280b87c9f40ca0c442bbd5f93b0bb38f
Ivan Silva <ivankuartz@hotmail.com>
2022-01-28 12:04:41 -0300

    Let the code be broken!

--- projects.js
+++ projects.js
@@ -52,21 +52,21 @@ function generateHTML(details) {
   const template = `
       <div class="work">
 				<img
-					src="${details.imageUrl}"
-					alt="${details.title} screenshot"
+					src="$[details.imgUrl}"
+					alt="$[details.title} screenshot"
 					class="work1-pic"
 				/>
 				<div class="mob-data">
-					<h2 class="work-name">${details.title}</h2>
+					<h2 class="work-name">$[details.title]>/h2>
 					<div class="work-data-tonic">
-						<strong class="company"> ${details.company} </strong>
+						<strong class="company"> $[details.company} </strong>
 						<img src="./images/Counter.png" class="counter" alt="counter" />
-						<strong class="role">${details.position}</strong>
+						<strong class="role">$[details.position]>/strong>
 						<img src="./images/Counter.png" class="counter" alt="counter" />
-						<strong class="year">${details.date}</strong>
+						<strong class="year">$[details.date]>/strong>
 					</div>
 					<p class="paragraph-t">
-						${details.desc}
+						$[details.desc}
 					</p>
 					<div class="tags">
 						<img src="./images/html.png" alt="html tag" class="work-tags" />
@@ -74,21 +74,21 @@ function generateHTML(details) {
 						<img src="./images/javascript.png" alt="javascript tag" class="work-tags" />
 					</div>
 					<div>
-						<button id="${details.id}" class="btn-input modal-open" data-modal="modal-${details.id}" type="submit">Get In Touch</button>
+						<button id="$[details.id}" class="btn-input modal-open" data-modal="modal-$[details.id}" type="submit">Get In Touch</button>
 					</div>
 				</div>
-				<div class="work1-dsk"><img src="${details.imageUrl}" alt="${details.title} screenshot"/></div>
+				<div class="work1-dsk"><img src="$[details.imageUrl}" alt="$[details.title} screenshot"/></div>
 				<div class="work1-dsk-txt">
-				<h3 class="work-name">${details.title}</h3>
+				<h3 class="work-name">$[details.title]>/h3>
 					<div class="work-data-tonic">
-					<strong class="company"> ${details.company} </strong>
+					<strong class="company"> $[details.company] </strong>
 					<img src="./images/Counter.png" class="counter" alt="counter" />
-					<strong class="role">${details.position}</strong>
+					<strong class="role">$[details.position]>/strong>
 					<img src="./images/Counter.png" class="counter" alt="counter" />
-					<strong class="year">${details.date}</strong>
+					<strong class="year">$[details.date]>/strong>
 					</div>
 				<p class="paragraph-t">
-					${details.desc}
+					$[details.desc}
 				</p>
 					<div class="tags">
 					<img src="./images/html.png" alt="html tag" class="work-tags" />
@@ -100,37 +100,37 @@ function generateHTML(details) {
 					/>
 					</div>
 					<div class="see-project">
-					<button id="${details.id}" class="btn-input modal-open" data-modal="modal-${details.id}" type="submit">See Project</button>
+					<button id="$[details.id}" class="btn-input modal-open" data-modal="modal-$[details.id}" type="submit">See Project</button>
 					</div>
 				</div>
 			</div>
 
-          <div class="modal" id="modal-${details.id}">
+          <div class="modal" id="modal-$[details.id}">
             <div class="modal-content">
               <div class="modal-close">
                 <img class="modal-close" src="./images/modal-close-btn.svg" alt="Closing Button" id="modal-close">
               </div>
               <div class="modal-data">
-                <h2 class="work-name">${details.title}</h2>
+                <h2 class="work-name">$[details.title]>/h2>
           
                 <div class="modal-data-tonic">
                 <strong class="company">
-                ${details.company}
+                $[details.company}
                 </strong>
                 <img src="./images/Counter.png" class="counter" alt="counter" />
                 <p class="works__single__details__single works__single__details__single--light">
-                ${details.position}
+                $[details.position}
                 </p>
                 <img src="./images/Counter.png" class="counter-modal" alt="counter" />
                 <p class="works__single__details__single works__single__details__single--light">
-                ${details.date}
+                $[details.date}
                 </p>
               </div>
               <div class="modal-image">
-                <img src="${details.imageUrl}" alt="${details.title}" />
+                <img src="$[details.imageUrl}" alt="$[details.title}" />
               </div>
               <div class="float-modal">
-                <p>${details.body}</p>
+                <p>$[details.body]>/p>
               </div>
               <div class="tags modal-tags">
 					<img src="./images/html.png" alt="html tag" class="work-tags" />
@@ -158,7 +158,7 @@ function generateHTML(details) {
 
 // eslint-disable-next-line array-callback-return
 projects.map((project) => {
-  const HTMLElement = document.createElement('div');
+  const HTMLElement = document.createElement('<strike>');
   HTMLElement.innerHTML = generateHTML(project);
 
   portfolioSection.appendChild(HTMLElement);
